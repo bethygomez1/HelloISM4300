de397128b7fe9d26b4f9a312cdb1fc2895e5ea0e
Gomez, Bethy <bethygomez@usf.edu>
2019-08-29 16:08:27 -0400

    We made some changes.

--- HelloISM4300/Program.cs
+++ HelloISM4300/Program.cs
@@ -6,7 +6,16 @@ namespace HelloISM4300
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+            // This is a change with 1 line comment
+            /* 
+             * This is a multi-line
+             * comment.
+            */
+            Console.WriteLine("This is a change.");
         }
     }
 }
